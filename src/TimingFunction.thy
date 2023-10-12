3a3fb787966edea3af7a8b03231d5e66a980d52a
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-10-12 10:25:50 +0200

    Move code to ML file

--- src/TimingFunction.thy
+++ src/TimingFunction.thy
@@ -3,20 +3,6 @@ theory TimingFunction
   keywords "define_time_fun" :: thy_decl
 begin
 
-ML \<open>
-fun print_cases cases =
-  (case cases of [] => () | (x::xs) => (tracing x; print_cases xs));
-
-fun convert thm_name func theory =
-let
-val ctxt = Proof_Context.init_global theory;
-val {case_names, ...} = Function.get_info ctxt (Syntax.read_term ctxt func)
-in (tracing ("cases of " ^ func ^ ":"); print_cases case_names; theory)
-end;
-
-Outer_Syntax.local_theory @{command_keyword "define_time_fun"}
-"Defines runtime function of a function"
-  (Parse_Spec.simple_spec >> (fn ((thm_name, _), func) => Local_Theory.background_theory (convert thm_name func)))
-\<close>
+ML_file TimingFunction.ML
 
 end
