41cf3755b4c95c383a3491eb3e6269293d2c1bb0
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-11-26 22:49:46 +0100

    Fix termination proof

---
+++ test/T_Check.thy
@@ -0,0 +1,9 @@
+theory T_Check
+  imports T_Binomial_Heap T_Braun_Trees T_Leftist_Heap
+    T_List T_Pairing_Heap_List1 T_Queue_2Lists T_Selection
+    T_Selection T_Skew_Heap T_Splay_Tree T_Tree23
+begin
+
+
+
+end
