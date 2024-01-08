7b911df1a0f23aaf2f92a6d4f90f68cb374f0831
nipkow <nipkow@in.tum.de>
2024-01-08 07:42:41 +0100

    comments

--- src/TimingFunction.thy
+++ src/TimingFunction.thy
@@ -1,3 +1,11 @@
+(*
+Author: Jonas Stahl
+
+Automatic definition of running time functions from HOL functions
+following the translation described in Section 1.5, Running Time, of the book
+Functional Data Structures and Algorithms. A Proof Assistant Approach.
+*)
+
 theory TimingFunction
   imports Main
   keywords "define_time_fun" :: thy_decl
@@ -9,6 +17,18 @@ ML_file ZeroFuns.ML
 ML_file TimingFunction.ML
 
 declare [[time_prefix = "T_"]]
+
+text \<open>The pre-defined functions below are assumed to have constant running time.
+In fact, we make that constant 0.
+This does not change the asymptotic running time of user-defined functions using the
+pre-defined functions because 1 is added for every user-defined function call.
+
+Note: Many of the functions below are polymorphic and reside in type classes.
+The constant-time assumption is justified only for those types where the hardware offers
+suitable support, e.g. numeric types. The argument size is implicitly bounded, too.
+
+The constant-time assumption for \<open>(=)\<close> is justified for recursive data types such as lists and trees
+as long as the comparison is of the form \<open>t = c\<close> where \<open>c\<close> is a constant term, for example \<open>xs = []\<close>.\<close>
 
 define_time_0 "(+)"
 define_time_0 "(-)"
